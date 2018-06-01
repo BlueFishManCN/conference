bab54d22210a1d8949197e53b2864c2dc87b02e2
mac-jerry <jerrychangcn@outlook.com>
2018-06-01 14:02:48 +0800

    sql

---
+++ sql/user.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50638
+ Source Host           : localhost:3306
+ Source Schema         : conference
+
+ Target Server Type    : MySQL
+ Target Server Version : 50638
+ File Encoding         : 65001
+
+ Date: 01/06/2018 14:02:09
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '姓名',
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '邮箱',
+  `phone` varchar(31) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '手机',
+  `country` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '国家',
+  `organization` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '单位',
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建状态',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新状态',
+  `is_delete` smallint(4) DEFAULT '0' COMMENT '删除状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9435388 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
