da0ce4de3a2afbf0d65a00427f978bd2afeed2fc
jerry's mac <jerrychangcn@outlook.com>
2019-07-18 21:22:01 +0800

    update readme

--- application/config/email.php
+++ application/config/email.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = '***';
-$config['smtp_user'] = '***@***.com';
+$config['smtp_user'] = '****@**.com';
 $config['smtp_pass'] = '***';
 $config['smtp_port'] = '465';
 $config['mailtype'] = 'html';
