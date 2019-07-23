9ba8fb2025109c8c279cf6afae210cb40c353a78
jerry's mac <jerrychangcn@outlook.com>
2019-07-23 16:36:14 +0800

    Update User.php

--- application/models/User.php
+++ application/models/User.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+/**
+ * Class User
+ */
 class User extends CI_Model
 {
     public function __construct()
@@ -9,6 +12,15 @@ class User extends CI_Model
         $this->load->database();
     }
 
+    /**
+     * @param $id
+     * @param $firstname
+     * @param $lastname
+     * @param $email
+     * @param $country
+     * @param $organization
+     * @param $password_hash
+     */
     public function insert($id, $firstname, $lastname, $email, $country, $organization, $password_hash)
     {
         $data = array(
@@ -23,6 +35,10 @@ class User extends CI_Model
         $this->db->insert("user", $data);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function getUserById($id)
     {
         $this->db->select('*');
@@ -34,17 +50,10 @@ class User extends CI_Model
         return $query;
     }
 
-    public function getUserByUsername($username)
-    {
-        $this->db->select('*');
-        $this->db->from('user');
-        $this->db->where('username', $username);
-        $this->db->where('is_delete', 0);
-        $this->db->order_by('updated_at', 'DESC');
-        $query = $this->db->get()->result();
-        return $query;
-    }
-
+    /**
+     * @param $email
+     * @return mixed
+     */
     public function getUserByEmail($email)
     {
         $this->db->select('*');
@@ -56,6 +65,10 @@ class User extends CI_Model
         return $query;
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function getEmailById($id)
     {
         $this->db->select('email');
@@ -67,6 +80,10 @@ class User extends CI_Model
         return $query[0]->email;
     }
 
+    /**
+     * @param $email
+     * @param $password_hash
+     */
     public function resetPassword($email, $password_hash)
     {
         $data = array(
@@ -76,5 +93,4 @@ class User extends CI_Model
         $this->db->where('is_delete', 0);
         $this->db->update('user', $data);
     }
-
 }
